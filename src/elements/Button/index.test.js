caf88e30d63e901be99de3a920d6f08bb9d646f0
alifanugrah045 <97005608+alifanugrah045@users.noreply.github.com>
2022-11-19 12:39:59 +0800

    CREATE FOLDER AND MORE

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import Button from "./Button";
+
+// test("should not allowed click button if isDisabled is present", () => {
+//   const { container } = render(<Button isDisabled></Button>);
+//   expect(container.querySelector("span.disabled")).toBeInTheDocument();
+// });
